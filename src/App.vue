041f9f56e94f00f8e9ff666be4650de3ee83d5c4
nik <nik@hal.com>
2021-04-14 14:38:25 +0300

    added main navigation tabs

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="main">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="tabs">
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/issues">Issues</router-link></li>
+          <li><router-link to="/pull-requests">Pull Requests</router-link></li>
+          <li><router-link to="/forks">Forks</router-link></li>
+        </ul>
+      </div>
     </div>
     <router-view />
   </div>
